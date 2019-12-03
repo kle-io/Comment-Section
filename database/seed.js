4e5fbcba919fda4b84b752aefb38c02313b0ee27
Alex Tian <alextian@Alexs-MacBook-Pro.local>
2019-12-02 18:37:43 -0800

    "changed faker syntax in seed"

--- database/seed.js
+++ database/seed.js
@@ -5,7 +5,7 @@ const faker = require('faker');
 const songData = [];
 for (var i = 0; i < 100; i++) {
   let song = {
-    artist: faker.fake({name.firstName} {name.lastName}),
+    artist: faker.name.firstName() + faker.name.lastName(),
     likes: Math.floor(Math.random() * 5,000 + 1,000),
     followers: Math.floor(Math.random() * 3,000 + 600),
     reposts: Math.floor(Math.random() * 800 + 160),
@@ -15,19 +15,19 @@ for (var i = 0; i < 100; i++) {
   let randomComs = Math.floor(Math.random() * 30)
   for (var j = 0; i < randomComs; j++) {
     let comment = {
-      user_photo: faker.fake({image.imageURL}),
-      username: faker.fake({internet.userName}),
-      time: faker.fake({date.recent}),
-      comment_body: faker.fake({lorem.sentence}),
+      user_photo: faker.image.imageURL(),
+      username: faker.internet.userName(),
+      time: faker.date.recent(),
+      comment_body: faker.lorem.sentence(),
       Replies: []
     }
     song.Comments.push(comment);
     let randomReps = Math.floor(Math.random() * 2)
     for (var k = 0; k < randomReps; k++) {
       let reply = {
-        username: faker.fake({internet.userName}),
-        user_photo: faker.fake({image.imageURL}),
-        reply_body: faker.fake({lorem.sentence})
+        username: faker.internet.userName(),
+        user_photo: faker.image.imageURL(),
+        reply_body: faker.lorem.sentence()
       }
       song.Comments[j].Replies.push(reply);
     }
